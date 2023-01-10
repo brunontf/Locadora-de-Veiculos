229ecc28d61f6e843ee39d72f424a2538b26160c
thi-costa <thiago14abr@gmail.com>
2023-01-10 16:04:37 -0300

    Visualização no console do model agência

--- App.java
+++ App.java
@@ -1,7 +1,28 @@
+import model.Agencia;
+import model.Endereco;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
+        List<Agencia> listaAgencias = new ArrayList<Agencia>();
+
+        Endereco endereco1 = new Endereco("Rua XV de novembro");
+        Endereco endereco2 = new Endereco("Rua Episcopal");
+        Agencia samamVeiculos1 = new Agencia("SAMAM Veículos", endereco1);
+        Agencia samamVeiculos2 = new Agencia("SAMAM Veículos", endereco2);
+
+        listaAgencias.add(samamVeiculos1);
+        System.out.println(temAgenciasDuplicadas(listaAgencias, samamVeiculos1));;
+        System.out.println(temAgenciasDuplicadas(listaAgencias, samamVeiculos2));
+
         
+    }
+
+    public static boolean temAgenciasDuplicadas(List<Agencia> listaAgencias, Agencia agencia){
+        return listaAgencias.stream().anyMatch(ag -> ag.equals(agencia));
     }
     
 }
