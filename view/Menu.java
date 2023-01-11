593c2e0d564cdceefa1fe4d3ca6e4f7846be25b1
brunontf <brunontf@gmail.com>
2023-01-11 00:10:10 -0300

    add Pessoa class

---
+++ view/Menu.java
@@ -0,0 +1,37 @@
+package view;
+
+import java.util.List;
+
+import model.Pessoa;
+import util.ConsoleUIHelper;
+
+public class Menu {
+    
+    public static void menu(List<Pessoa> listaClientes) {
+        int opcao = ConsoleUIHelper.askChooseOption("Digite a operação desejada",
+         "Menu clientes",
+          "Menu veiculos",
+          "Menu agencias",
+          "Menu alugueis",
+          "Sair");
+
+        switch (opcao) {
+            case 0 -> { // chama menu pessoa
+                MenuPessoa.menu(listaClientes);
+            }
+            case 1 -> { // sair
+                System.exit(0);
+            }
+            case 2 -> { // sair
+                System.exit(0);
+            }
+            case 3 -> { // sair
+                System.exit(0);
+            }
+            case 4 -> { // sair
+                System.exit(0);
+            }
+
+        }
+    }
+}
