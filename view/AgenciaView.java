9f4752a1ea39970767996b8a77a948ba567eaa4b
thi-costa <thiago14abr@gmail.com>
2023-01-14 12:09:03 -0300

    AgenciaView - implemented 'menu', 'adicionar', 'buscar' and 'listar'

---
+++ view/AgenciaView.java
@@ -0,0 +1,80 @@
+package view;
+
+import controller.AgenciaController;
+import model.Agencia;
+import model.Endereco;
+import util.ConsoleUIHelper;
+
+import java.util.List;
+
+public class AgenciaView {
+    public static void menu(){
+        AgenciaController agenciaController = new AgenciaController();
+
+        int opcao = ConsoleUIHelper.askChooseOption("Digite a opção desejada",
+                "Adicionar uma nova agência",
+                "Editar agência",
+                "Buscar agência",
+                "Listar agências",
+                "Retornar ao menu"
+                );
+        switch (opcao){
+            case 0 -> agenciaController.adicionar(null);
+            case 1 -> agenciaController.editar(null);
+            case 2 -> agenciaController.buscar(null, null);
+            case 3 -> agenciaController.listar();
+        }
+    }
+
+    public static void adicionar(){
+        Agencia agencia = new Agencia();
+        Endereco endereco = new Endereco();
+        AgenciaController agenciaController = new AgenciaController();
+
+        String nome = ConsoleUIHelper.askSimpleInput("Digite o nome da agência");
+        agencia.setNome(nome);
+
+        String cep = ConsoleUIHelper.askSimpleInput("Digite seu cep").replace("-", "");
+        String logradouro = ConsoleUIHelper.askSimpleInput("Digite seu logradouro");
+        String numero = ConsoleUIHelper.askSimpleInput("Digite seu numero");
+        String cidade = ConsoleUIHelper.askSimpleInput("Digite sua cidade");
+        String estado = ConsoleUIHelper.askSimpleInput("Digite seu estado");
+
+        endereco.setCep(cep);
+        endereco.setLogradouro(logradouro);
+        endereco.setNumero(numero);
+        endereco.setCidade(cidade);
+        endereco.setEstado(estado);
+
+        agencia.setEndereco(endereco);
+
+        agenciaController.adicionar(agencia);
+    }
+
+    public static void editar() {
+        String nome = ConsoleUIHelper.askSimpleInput("Digite o nome da agência");
+        String logradouro = ConsoleUIHelper.askSimpleInput("Digite seu logradouro");
+    }
+
+    public static void buscar() {
+        AgenciaController agenciaController = new AgenciaController();
+        String nome = ConsoleUIHelper.askSimpleInput("Digite o nome da agência");
+        String logradouro = ConsoleUIHelper.askSimpleInput("Digite seu logradouro");
+
+        agenciaController.buscar(nome, logradouro);
+
+    }
+
+    public static void listar(List<Agencia> agencias) {
+        int i = 0;
+        for(Agencia agencia: agencias){
+            System.out.println(i + " - " + agencia.toString());
+            i++;
+        }
+        System.out.println();
+    }
+
+
+
+
+}
