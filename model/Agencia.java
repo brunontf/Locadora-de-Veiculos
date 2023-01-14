e344d101a05159971547110866ccfbc1a10244c0
thi-costa <thiago14abr@gmail.com>
2023-01-14 12:07:17 -0300

    Agencia Model - refactor equals and implemented method toString

--- model/Agencia.java
+++ model/Agencia.java
@@ -40,11 +40,17 @@ public class Agencia {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Agencia agencia = (Agencia) o;
-        return Objects.equals(nome, agencia.nome) && Objects.equals(endereco, agencia.endereco);
+        return Objects.equals(nome.toLowerCase(), agencia.nome.toLowerCase())
+                && Objects.equals(endereco, agencia.endereco);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(nome, endereco);
+    }
+
+    @Override
+    public String toString() {
+        return "nome='" + nome +", "+"endereço=" + endereco ;
     }
 }
